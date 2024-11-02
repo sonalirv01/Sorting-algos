9473e52cf6d4ac4b3f33705e10b7a283b5d2eca1
sonalirv01 <sonalisv01@gmail.com>
2024-11-02 19:04:22 +0530

    first commint

---
+++ src/BubbleSort.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;public class BubbleSort{
+
+
+     static void bubbleSorting(int[] arr)
+    {
+        boolean swaping;
+
+        for (int i = 0; i < arr.length; i++) {
+            swaping=false;
+
+            for (int j = 1; j < arr.length - i; j++) {
+                if(arr[j] < arr[j-1])
+                {
+                    int temp = arr[j];
+                    arr[j]= arr[j-1];
+                    arr[j-1] = temp;
+                    swaping=true;
+                }
+            }
+            if(!swaping)
+            {
+                break;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] arr={3,4,6,5,7,2,1};
+        bubbleSorting(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
